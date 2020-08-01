95898eb8e0f66b52d11b0bd81fd1178f14781221
chengzao <czhlink@163.com>
2020-08-01 17:59:57 +0800

    update index

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ fetcher({ login: USER }, TOKEN)
 
   async function publishReadme(){
     await exec.exec('git', ['--version']);
-    await exec.exec('git', ['add', 'README.md']);
+    await exec.exec('git', ['config', '--global', 'user.name', 'chengzao']);
+    await exec.exec('git', ['config', '--global', 'user.email', 'czhlink@163.com']);
     await exec.exec('git', ['commit','-am', 'Update: README.md']);
     await exec.exec('git', ['push', 'origin', 'master']);
   }
