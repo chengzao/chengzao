b2ddcb0c08e915bfbffce6555cf5005e6c9ade59
chengzao <czhlink@163.com>
2020-08-01 20:18:15 +0800

    update index

--- index.js
+++ index.js
@@ -7,28 +7,51 @@ const exec = require('@actions/exec');
 require("dotenv").config();
 
 // @url: https://developer.github.com/v4/explorer/
-const GITHUB_URL = "https://api.github.com/graphql";
+const GH_API = "https://api.github.com/graphql";
 
 // @url: https://github.com/settings/tokens
 const TOKEN = process.env.ACCESS_TOKEN;
-const USER = "chengzao";
 const GH_REF = "github.com/chengzao/chengzao.git";
-
 const tpl = path.join(__dirname, "./template/README.md");
 const outputDir = path.join(__dirname, "./output/README.md");
 const rootDir = path.join(__dirname, "./README.md");
 
-// read file
+// read readme tpl file
 const tplContent = fs.readFileSync(tpl, { encoding: "utf-8" });
 
 function request(data, headers = {}) {
   return axios({
-    url: GITHUB_URL,
+    url: GH_API,
     method: "post",
     headers,
     data,
   });
 }
+
+// @url:https://aui.github.io/art-template/zh-cn/docs/syntax.html
+template.defaults.imports.dateFormat = function (time, fmt) {
+  var date = new Date(time)
+  var o = {
+    'Y+': date.getFullYear(), // year
+    'M+': date.getMonth() + 1, // month
+    'D+': date.getDate(), // day
+    'h+': date.getHours(), // hour
+    'm+': date.getMinutes(), // minutes
+    's+': date.getSeconds(), // seconds
+    'q+': Math.floor((date.getMonth() + 3) / 3), // quarterly
+    S: date.getMilliseconds(), // Millisecond
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        o[k] < 10 ? '0' + o[k] : o[k],
+      )
+    }
+  }
+  return fmt
+};
+
 
 const fetcher = (variables, token) => {
   return request(
@@ -36,7 +59,6 @@ const fetcher = (variables, token) => {
       query: `
       query userInfo($login: String!) {
         user(login: $login) {
-            bio
             url
         }
         viewer {
@@ -59,27 +81,27 @@ const fetcher = (variables, token) => {
   );
 };
 
-fetcher({ login: USER }, TOKEN)
+fetcher({ login: 'chengzao' }, TOKEN)
   .then((res) => {
     const rs = res.data.data;
     const repositories = rs.viewer.repositories;
+    const runTime = new Date()
 
-    // render data
+    // template render data
     const outputContent = template.render(tplContent, {
       url: rs.user.url,
-      user: USER,
-      bio: rs.user.bio,
       nodes: repositories.nodes,
+      runTime: runTime
     });
 
-    // write file
+    // write readme.md content
     fs.writeFileSync(outputDir, outputContent, { encoding: "utf-8" });
 
     // exist file
     const isExistsFile = fs.existsSync(outputDir);
 
     if (isExistsFile) {
-      // copy file
+      // copy output/readme.md file to root dir
       fs.copyFileSync(outputDir, rootDir);
 
       try {
