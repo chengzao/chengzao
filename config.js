2f96b106627d5935c451d55459cff16729405643
chenzhaohong <chenzhaohong@tal.com>
2021-06-11 15:52:27 +0800

    Update

--- config.js
+++ config.js
@@ -1,38 +1,41 @@
-const GITEE_BASE = 'https://gitee.com/cxyz/imgbed/raw/img'
-
+// const GITEE_BASE = 'https://gitee.com/cxyz/imgbed/raw/img'
 // @url: https://github.com/topics
-const TOPICS_BASE = 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics'
+// const TOPICS_BASE = 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics'
+
+const TOPICS_BASE =
+  "https://cdn.jsdelivr.net/gh/chengzao/chengzao@dev/icons";
+
 const TOPICS = [
   {
-    url: `${TOPICS_BASE}/html/html.png`,
+    url: `${TOPICS_BASE}/html.png`,
     name: 'html'
   },
   {
-    url: `${TOPICS_BASE}/javascript/javascript.png`,
+    url: `${TOPICS_BASE}/javascript.png`,
     name: 'javascript'
   },
   {
-    url: `${TOPICS_BASE}/vue/vue.png`,
+    url: `${TOPICS_BASE}/vue.png`,
     name: 'vue'
   },
   {
-    url: `${TOPICS_BASE}/react/react.png`,
+    url: `${TOPICS_BASE}/react.png`,
     name: 'react'
   },
   {
-    url: `${GITEE_BASE}/2020/08/wxminapp.png`,
+    url: `${GITEE_BASE}/wxminapp.png`,
     name: '微信小程序'
   },
   {
-    url: `${TOPICS_BASE}/nodejs/nodejs.png`,
+    url: `${TOPICS_BASE}/nodejs.png`,
     name: 'nodejs'
   },
   {
-    url: `${TOPICS_BASE}/webpack/webpack.png`,
+    url: `${TOPICS_BASE}/webpack.png`,
     name: 'webpack'
   },
   {
-    url: `${TOPICS_BASE}/git/git.png`,
+    url: `${TOPICS_BASE}/git.png`,
     name: 'git'
   },
 ]
