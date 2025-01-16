b8a7c3271ac06a0431697ba08c87f64af2d65d56
chengzao <czhlink@163.com>
2025-01-16 10:39:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function run() {
       let nodes = repositories.nodes.filter(item => item.name !== 'chengzao')
       nodes = nodes.length > 3 ? nodes.slice(0,3) : nodes;
       nodes = nodes.map(item => {
-        console.log('name: ', item.name)
+        console.log('repo name: ', item.name, 'pushedAt:', item.pushedAt)
         return {
           name: item.name,
           url: item.url,
